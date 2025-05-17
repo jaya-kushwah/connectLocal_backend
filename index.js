1710954c3f42d0ba54c132e6fbf4e61a10de4247
jaya-kushwah <jayakushwah2006@gmail.com>
2025-05-17 21:55:04 +0530

    add

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
-
+// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true
-// }));
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
 var path = require('path');
 app.use("/event/upload", express.static(path.join(__dirname, 'upload')));
 app.use("/user/upload", express.static(path.join(__dirname, 'upload')));
